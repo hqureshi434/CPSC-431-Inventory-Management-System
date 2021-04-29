f91ab083f509a0d72365e83d20422b83527c888e
hqureshi434 <hqureshi434@gmail.com>
2021-04-28 23:37:28 -0700

    Made changes to the login.php file. Still do not know if it works properly.

--- Login.php
+++ Login.php
@@ -4,12 +4,12 @@
   ini_set('display_errors', 1);
 ?>
 
-<div class="container">
       <?php
-        if(isset($_POST['logInBtn'])) {
-          authUserLogin();
+        if(isset($_POST['loginBut'])) {
+          //authUserLogin();
+          login($userName, $userPassword);
         }
-
+        /*
         function authUserLogin() {
           @$db = new mysqli('mariadb', 'cs431s23', 'Va7Wobi9', 'cs431s23');
 
@@ -22,9 +22,9 @@
 
           $query = mysqli_query($db, "SELECT * FROM IMSLogin WHERE userName='" . $_POST["Username"] . "' and userPassword = '". $_POST["Password"]."'");
           $numRows = mysqli_num_rows($query);
-          /*$stmt = $db->prepare($query);
+          $stmt = $db->prepare($query);
           $stmt->bind_param('sssss', $photoName, $photoDate, $photographer, $location, $fileToUpload);
-          $stmt->execute();*/
+          $stmt->execute();
 
           if ($numRows == 0) {
               echo  "<p>Username or Password does not exist.</p>";
@@ -32,6 +32,35 @@
               echo "<p>Successfully logged in.</p>";
           }
           $db->close();     
-        }
+        }*/
+        
+        function login($username, $userpassword) {
+          // check userName$userName and userPas$userPassword with db
+          // if yes, return true
+          // else throw exception
+          
+            // connect to db
+            $db = new mysqli('mariadb', 'cs431s23', 'Va7Wobi9', 'cs431s23');
+            // Check database connection
+            if (mysqli_connect_errno()) {
+              echo "<p>Error: Could not connect to database.<br/>
+                  Please try again later.</p>";
+              exit;
+            }
+
+            // check if userName$userName is unique
+            $result = $db->query("select * from IMSlogin
+                                   where userName='".$username."'
+                                   and userPassword = sha1('".$userpassword."')");
+            if (!$result) {
+               throw new Exception('Could not log you in.');
+            }
+          
+            if ($result->num_rows>0) {
+               return true;
+            } else {
+               throw new Exception('Could not log you in.');
+            }
+            echo '<p>success</p>';
+          }
       ?>
-    </div>
--- Login.sql
+++ Login.sql
@@ -1,4 +1,13 @@
-CREATE TABLE IMSLogin
-( userName varchar(255) NOT NULL,
-  userPassword varchar(255) NOT NULL
+CREATE TABLE IMSlogin
+( 
+  userName varchar(16) primary key,
+  userPassword char(40) not null,
+  Email varchar(100) not null
+);
+
+CREATE TABLE IMSitem
+(
+  itemName varchar(40) primary key,
+  itemPrice float(4, 2) not null,
+  itemQty  tinyint unsigned not null
 );
